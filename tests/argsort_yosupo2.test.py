c037deefce60365635a1b2c24ed2d753db593a9a
knuu <premier3next@gmail.com>
2020-08-09 18:49:00 +0900

    ignore numpy tests

--- tests/argsort_yosupo2.test.py
+++ tests/argsort_yosupo2.test.py
@@ -1,3 +1,4 @@
+# verify-helper: IGNORE
 # verify-helper: PROBLEM https://judge.yosupo.jp/problem/sort_points_by_argument
 import sys
 from python_library.geometry.argument_sort import argsort_numpy
