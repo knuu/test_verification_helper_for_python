22cfe9818e664f48d7a09909e7eabe83d6f4ecb7
knuu <premier3next@gmail.com>
2020-08-09 19:42:08 +0900

    remove main

--- python_library/geometry/argument_sort.py
+++ python_library/geometry/argument_sort.py
@@ -38,24 +38,3 @@ def argsort(points: List[Tuple[float, float]]
     uppers.sort(key=cmp_to_key(compare))
     origins.sort(key=cmp_to_key(compare))
     return itertools.chain(lowers, origins, uppers)
-
-
-def yosupo():
-    # https://judge.yosupo.jp/problem/sort_points_by_argument
-    N = int(input())
-    points = [tuple(int(val) for val in input().split()) for _ in range(N)]
-    for x, y in argsort_numpy(points):
-        print(x, y)
-
-
-def yosupo2():
-    # https://judge.yosupo.jp/problem/sort_points_by_argument
-    N = int(input())
-    points = [tuple(int(val) for val in input().split()) for _ in range(N)]
-    for x, y in argsort(points):
-        print(x, y)
-
-
-if __name__ == "__main__":
-    # yosupo()
-    yosupo2()
